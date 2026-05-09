11cba3675a3aab1095b064ca94c6f4a4d43c50fe
Samina-Kapasi <kapasisamina780@gmail.com>
2026-05-09 18:33:52 +0530

    Added notes API

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+import json
 
 app= FastAPI()
+
+def load_data():
+    with open('notes.json', 'r') as f:
+        data= json.load(f)
+    return data
+
 
 @app.get("/")
 def home():
     return {"message":"Home URL"}
+
+
+@app.get("/notes")
+def view_notes():
+    data=load_data()
+    return data
+
+
 

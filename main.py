281dfb2a8ea5bf3d4902cd4665f9062cf4358aaf
Samina-Kapasi <kapasisamina780@gmail.com>
2026-05-12 15:25:23 +0530

    updated API

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app= FastAPI()
@@ -19,5 +19,27 @@ def view_notes():
     data=load_data()
     return data
 
+@app.get("/notes/{note_id}")
+def notes_id(note_id: str):
+    data= load_data()
 
+    if note_id not in data:
+        raise HTTPException(status_code=401, detail="Note not found")
+    
+    return data[note_id]
 
+@app.get("/sort_notes")
+def sort_notes(order:str = Query(description="[asc, desc]")):
+    data= load_data()
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail="Invalid choose")
+    
+    if order=='desc':
+        sort_order=True
+    else:
+        sort_order=False
+    
+    sorted_data=sorted(data.items(), key=lambda x: x[0], reverse=sort_order)
+
+    return dict(sorted_data)

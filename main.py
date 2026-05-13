91404c8471c34a457de807cd720d8296f96432dd
Samina-Kapasi <kapasisamina780@gmail.com>
2026-05-13 15:25:58 +0530

    Update and Delete end-point created

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Path, Query
 import json
 from pydantic import BaseModel, Field
-from typing import Annotated, Literal
+from typing import Annotated, Optional
 from fastapi.responses import JSONResponse
 
 app= FastAPI()
@@ -12,7 +12,15 @@ class Notes(BaseModel):
     content:Annotated[str, Field(..., description="Content of note")]
     category:Annotated[str, Field(..., description="Category of note")]
     author:Annotated[str, Field(..., description="Author of the note")]
-    completed:Annotated[bool, Field( default=False,description="Completion status of notes")]
+    completed:Annotated[bool, Field( default=False,description="Completion status of note")]
+
+class Updated_notes(BaseModel):
+    title:Annotated[str, Optional, Field(description="Note title")]
+    content:Annotated[str, Optional, Field( description="Note content")]
+    category:Annotated[str, Optional, Field(description="Note category")]
+    author:Annotated[str, Optional, Field(description="Author of the note")]
+    completed:Annotated[bool, Optional, Field(description="Completion status of node")]
+
 
 def load_data():
     with open('notes.json', 'r') as f:
@@ -59,16 +67,42 @@ def sort_notes(order:str = Query(description="[asc, desc]")):
 
     return dict(sorted_data)
 
-@app.post("/create")
+@app.post("/notes")
 def create(notes:Notes):
     data=load_data()
 
     if notes.id in data:
-        raise HTTPException(status_code=401, detail="Note already existed")
+        raise HTTPException(status_code=409, detail="Note already existed")
     
     data[notes.id]=notes.model_dump(exclude=['id'])
 
     save_data(data)
 
-    return JSONResponse(status_code=201, content="Note addedd successfully")
+    return JSONResponse(status_code=201, content={"message":"Note added successfully"})
 
+@app.delete("/delete/{note_id}")
+def delete(note_id:str= Path(..., description="Note ID", examples=["N001"])):
+    data=load_data()
+
+    if note_id not in data:
+        raise HTTPException(status_code=404, detail="Note ID not found")
+    
+    del data[note_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message":"Note ID deleted successfully"})
+
+
+@app.put("notes/{note_id}")
+def update(note_id:str, update_note:Updated_notes):
+    data=load_data()
+
+    if note_id not in data:
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    data[note_id]=update_note.model_dump(exclude=['id'])
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message":"Updated successfully"})
